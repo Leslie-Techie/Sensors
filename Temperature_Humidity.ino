2ca68876685b9edc6a806072f996107f0983b2d8
JARED LESLIE OLUBAYI <124913368+Leslie-Techie@users.noreply.github.com>
2023-11-21 10:16:52 +0300

    Add files via upload

---
+++ Temperature_Humidity.ino
@@ -0,0 +1,28 @@
+#include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+ {
+ dht.begin();
+ lcd.backlight();
+ lcd.init();
+}
+void loop() {
+delay(2000);
+ lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print("Humidity=");
+   lcd.print((float)dht.readHumidity());
+   lcd.print("%");
+   lcd.setCursor(0,1);
+   lcd.print("Temp=");
+   lcd.print((float)dht.readTemperature());
+   lcd.print("Celsius");
+   delay(2000);
+
+}
